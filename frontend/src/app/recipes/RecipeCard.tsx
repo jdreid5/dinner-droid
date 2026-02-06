1cba4bdd939305ab7937d0bb4b7cdca5ffa56c2f
jdreid5 <102168095+jdreid5@users.noreply.github.com>
2026-02-06 18:13:41 +0000

    move recipe card tsx into its own file

--- frontend/src/app/recipes/RecipeCard.tsx
+++ frontend/src/app/recipes/RecipeCard.tsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
+import { Recipe } from "../types/recipe";
 
-export default function RecipeCard() {
-
+export default function RecipeCard({ recipe }: { recipe: Recipe}) {
+	return (
+		<li key={recipe.id} className="grid grid-rows-[16rem_4rem_1.5rem] items-center">
+			<img src={recipe.imageUrl || ""} alt={recipe.title} className="h-full aspect-square rounded-lg mx-auto"/>
+			<h2 className="text-lg text-gray-700 text-center line-clamp-2">{recipe.title}</h2>
+			<p className="text-sm text-gray-500 text-center">{recipe.cookMinutes} minutes</p>
+		</li>
+	)
 }
--- frontend/src/app/recipes/page.tsx
+++ frontend/src/app/recipes/page.tsx
@@ -1,12 +1,30 @@
 import { getRecipes, getSearchedRecipes } from "@/lib/api";
 import SearchBar from "./SearchBar";
+import RecipeCard from "./RecipeCard";
+import { Recipe } from "../types/recipe";
 import { Suspense } from "react";
 
 export default async function RecipesPage({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }>}) {
 	const { q } = await searchParams;
 	const searchTerm = typeof q === "string" ? q : "";
-	const recipes = searchTerm ? await getSearchedRecipes(searchTerm) :await getRecipes();
-	
+	const recipes = searchTerm ? await getSearchedRecipes(searchTerm) : await getRecipes();
+
+	function RenderRecipes () {
+		if (recipes.length) {
+			return (
+				<ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+					{recipes.map((recipe: Recipe) => (
+						<RecipeCard key={recipe.id} recipe={recipe} />
+					))}
+				</ul>
+			)
+		} else {
+			return (
+				<p className="text-center text-gray-500">We couldn't find any recipes matching your search. Try something else!</p>
+			)
+		}
+	}
+
 	return (
 		<div>
 			<div id="search-bar" className="flex justify-center items-center">
@@ -15,15 +33,7 @@ export default async function RecipesPage({ searchParams }: { searchParams: Prom
 				</Suspense>
 			</div>
 			<br />
-			<ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
-				{recipes.map((recipe) => (
-					<li key={recipe.id} className="grid grid-rows-[16rem_4rem_1.5rem] items-center">
-						<img src={recipe.imageUrl || ""} alt={recipe.title} className="h-full aspect-square rounded-lg mx-auto"/>
-						<h2 className="text-lg text-gray-700 text-center line-clamp-2">{recipe.title}</h2>
-						<p className="text-sm text-gray-500 text-center">{recipe.cookMinutes} minutes</p>
-					</li>
-				))}
-			</ul>
+			<RenderRecipes />
 		</div>
 	)
 }
