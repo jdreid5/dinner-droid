f1898e2936dd09ac84463baeeaec3547f8b3fb33
jdreid5 <102168095+jdreid5@users.noreply.github.com>
2026-02-12 21:21:43 +0000

    add basic recipe page structure

---
+++ frontend/src/app/recipes/[id]/page.tsx
@@ -0,0 +1,60 @@
+import { getRecipe } from "@/lib/api";
+
+export default async function RecipePage({ params }: { params: Promise<{ id: string }> }) {
+	const { id } = await params;
+	const recipe = await getRecipe(id);
+	
+	return (
+		<div>
+			<h1>{recipe.title}</h1>
+			<p>{recipe.cookMinutes} minutes</p>
+			<img src={recipe.imageUrl || ""} alt={`aerial photo of ${recipe.title}`} />
+			<h2>Ingredients</h2>
+			<table>
+				{recipe.ingredients?.map((ingredient) => {
+					return (
+						<tr>
+							<td>{ingredient.name}</td>
+							<td>{ingredient.qty}{ingredient.unit}</td>
+						</tr>
+					)
+				})}
+			</table>
+			<h2>Steps</h2>
+			<ol>
+				{recipe.steps?.map((step) => {
+					return (
+						<li>{step.body}</li>
+					)
+				})}
+			</ol>
+			<h2>Nutritional Information</h2>
+			<table>
+				<tr>
+					<td>Calories</td>
+					<td>{recipe.calories}kcal</td>
+				</tr>
+				<tr>
+					<td>Protein</td>
+					<td>{recipe.protein}g</td>
+				</tr>
+				<tr>
+					<td>Carbohydrate</td>
+					<td>{recipe.carbohydrate}g</td>
+				</tr>
+				<tr>
+					<td>Fat</td>
+					<td>{recipe.fat}g</td>
+				</tr>
+				<tr>
+					<td>Fibre</td>
+					<td>{recipe.fibre}g</td>
+				</tr>
+				<tr>
+					<td>Salt</td>
+					<td>{recipe.salt}g</td>
+				</tr>
+			</table>
+		</div>
+	)
+}
