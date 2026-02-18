c2b5ef241d8c71a56a6c1d0a1be659bab3f2b9aa
jdreid5 <102168095+jdreid5@users.noreply.github.com>
2026-02-18 15:53:03 +0000

    add link to recipeCard component

--- frontend/src/app/recipes/RecipeCard.tsx
+++ frontend/src/app/recipes/RecipeCard.tsx
@@ -4,9 +4,11 @@ import { Recipe } from "../types/recipe";
 export default function RecipeCard({ recipe }: { recipe: Recipe}) {
 	return (
 		<li key={recipe.id} className="grid grid-rows-[16rem_4rem_1.5rem] items-center">
-			<img src={recipe.imageUrl || ""} alt={recipe.title} className="h-full aspect-square rounded-lg mx-auto"/>
-			<h2 className="text-lg text-gray-700 text-center line-clamp-2">{recipe.title}</h2>
-			<p className="text-sm text-gray-500 text-center">{recipe.cookMinutes} minutes</p>
+			<Link href={`/recipes/${recipe.id}`} className="contents">
+				<img src={recipe.imageUrl || ""} alt={recipe.title} className="h-full aspect-square rounded-lg mx-auto"/>
+				<h2 className="text-lg text-gray-700 text-center line-clamp-2">{recipe.title}</h2>
+				<p className="text-sm text-gray-500 text-center">{recipe.cookMinutes} minutes</p>
+			</Link>
 		</li>
 	)
 }
