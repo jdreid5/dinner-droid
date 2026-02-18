bde15a67124cccc56209b60e54a908b258cda41c
jdreid5 <102168095+jdreid5@users.noreply.github.com>
2026-02-18 16:05:53 +0000

    add keys to mapped children and wrap tables in tbody elements

--- frontend/src/app/recipes/[id]/page.tsx
+++ frontend/src/app/recipes/[id]/page.tsx
@@ -11,49 +11,53 @@ export default async function RecipePage({ params }: { params: Promise<{ id: str
 			<img src={recipe.imageUrl || ""} alt={`aerial photo of ${recipe.title}`} />
 			<h2>Ingredients</h2>
 			<table>
-				{recipe.ingredients?.map((ingredient) => {
-					return (
-						<tr>
-							<td>{ingredient.name}</td>
-							<td>{ingredient.qty}{ingredient.unit}</td>
-						</tr>
-					)
-				})}
+				<tbody>
+					{recipe.ingredients?.map((ingredient) => {
+						return (
+							<tr key={ingredient.name}>
+								<td>{ingredient.name}</td>
+								<td>{ingredient.qty}{ingredient.unit}</td>
+							</tr>
+						)
+					})}
+				</tbody>
 			</table>
 			<h2>Steps</h2>
 			<ol>
 				{recipe.steps?.map((step) => {
 					return (
-						<li>{step.body}</li>
+						<li key={step.n}>{step.body}</li>
 					)
 				})}
 			</ol>
 			<h2>Nutritional Information</h2>
 			<table>
-				<tr>
-					<td>Calories</td>
-					<td>{recipe.calories}kcal</td>
-				</tr>
-				<tr>
-					<td>Protein</td>
-					<td>{recipe.protein}g</td>
-				</tr>
-				<tr>
-					<td>Carbohydrate</td>
-					<td>{recipe.carbohydrate}g</td>
-				</tr>
-				<tr>
-					<td>Fat</td>
-					<td>{recipe.fat}g</td>
-				</tr>
-				<tr>
-					<td>Fibre</td>
-					<td>{recipe.fibre}g</td>
-				</tr>
-				<tr>
+				<tbody>
+					<tr>
+						<td>Calories</td>
+						<td>{recipe.calories}kcal</td>
+					</tr>
+					<tr>
+						<td>Protein</td>
+						<td>{recipe.protein}g</td>
+					</tr>
+					<tr>
+						<td>Carbohydrate</td>
+						<td>{recipe.carbohydrate}g</td>
+					</tr>
+					<tr>
+						<td>Fat</td>
+						<td>{recipe.fat}g</td>
+					</tr>
+					<tr>
+						<td>Fibre</td>
+						<td>{recipe.fibre}g</td>
+					</tr>
+					<tr>
 					<td>Salt</td>
-					<td>{recipe.salt}g</td>
-				</tr>
+						<td>{recipe.salt}g</td>
+					</tr>
+				</tbody>
 			</table>
 		</div>
 	)
